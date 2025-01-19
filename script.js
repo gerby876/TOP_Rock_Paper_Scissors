85231c483a202770cdb1270dc83c01f6432a0429
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-01-19 11:31:39 -0500

    Add playround function to allow for outcomes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function getComputerChoice() {
    let x = Math.floor(Math.random() * 3);
    if (x === 0) {
-    computerChoice = 'Rock';
+    computerChoice = Rock;
    } else if (x === 1) {
-    computerChoice = 'Paper';
+    computerChoice = Paper;
    } else {
-    computerChoice = 'Scissors';
+    computerChoice = Scissors;
    }
    return computerChoice;
 }
@@ -15,11 +15,11 @@ let toss = prompt ('Choose Rock Paper or Scissors', 'Rock');
 
 function getHumanChoice(toss) {
     if (toss === 'Rock') {
-        humanChoice = 'Rock'
+        humanChoice = Rock
     } else if (toss === 'Paper') {
-        humanChoice = 'Paper'
+        humanChoice = Paper
     } else {
-        humanChoice = 'Scissor'
+        humanChoice = Scissors
     }
     return humanChoice;
 }
@@ -27,3 +27,15 @@ console.log(getHumanChoice(toss))
 
 let humanScore = Number(0)
 let computerScore = Number(0)
+
+function playround(humanChoice, computerChoice) {
+        if (humanChoice === Rock && computerChoice === Scissors || humanChoice === Paper && computerChoice === Rock || humanChoice === Scissors && computerChoice === Paper) {
+            outcome = 'You win, ${humanChoice} beat ${computerChoice}.'
+        } else if (humanChoice === 'Rock' && computerChoice === 'Paper' || humanChoice === 'Paper' && computerChoice === 'Scissors' || humanChoice === 'Scissors' && computerChoice === 'Rock') {
+            outcome = 'You lose, ${computerChoice} beat ${humanChoice}.'
+        } else { outcome = 'It is a draw try again.'}
+        return outcome
+
+}
+
+console.log(playround(humanChoice, computerChoice))
