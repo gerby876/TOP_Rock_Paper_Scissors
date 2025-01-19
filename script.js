e146fa35a7aaec774344891b771789045973445d
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-01-19 11:40:29 -0500

    Add outcomes to the playround feature remove logs for human and computer choice fix outcome to display human and computer choice

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 function getComputerChoice() {
    let x = Math.floor(Math.random() * 3);
    if (x === 0) {
-    computerChoice = Rock;
+    computerChoice = 'Rock';
    } else if (x === 1) {
-    computerChoice = Paper;
+    computerChoice = 'Paper';
    } else {
-    computerChoice = Scissors;
+    computerChoice = 'Scissors';
    }
    return computerChoice;
 }
-console.log(getComputerChoice());
+
+getComputerChoice()
 
 let toss = prompt ('Choose Rock Paper or Scissors', 'Rock');
 
 function getHumanChoice(toss) {
     if (toss === 'Rock') {
-        humanChoice = Rock
+        humanChoice = 'Rock'
     } else if (toss === 'Paper') {
-        humanChoice = Paper
+        humanChoice = 'Paper'
     } else {
-        humanChoice = Scissors
+        humanChoice = 'Scissors'
     }
     return humanChoice;
 }
-console.log(getHumanChoice(toss))
+
+getHumanChoice(toss)
 
 let humanScore = Number(0)
 let computerScore = Number(0)
 
 function playround(humanChoice, computerChoice) {
-        if (humanChoice === Rock && computerChoice === Scissors || humanChoice === Paper && computerChoice === Rock || humanChoice === Scissors && computerChoice === Paper) {
-            outcome = 'You win, ${humanChoice} beat ${computerChoice}.'
+        if (humanChoice === 'Rock' && computerChoice === 'Scissors' || humanChoice === 'Paper' && computerChoice === 'Rock' || humanChoice === 'Scissors' && computerChoice === 'Paper') {
+            outcome = 'You win, ' + humanChoice + ' beats ' + computerChoice + '.'
         } else if (humanChoice === 'Rock' && computerChoice === 'Paper' || humanChoice === 'Paper' && computerChoice === 'Scissors' || humanChoice === 'Scissors' && computerChoice === 'Rock') {
-            outcome = 'You lose, ${computerChoice} beat ${humanChoice}.'
+            outcome = 'You lose,' + computerChoice + ' beats ' + humanChoice + '.'
         } else { outcome = 'It is a draw try again.'}
         return outcome
 
