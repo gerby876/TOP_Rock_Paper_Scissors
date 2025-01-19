edc312eb7d3d1fc188e9015ff992fc5dbe8acd80
Eric Gerberick <188939751+gerby876@users.noreply.github.com>
2025-01-19 10:15:51 -0500

    Implement variables for computer and human choices

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 function getComputerChoice() {
    let x = Math.floor(Math.random() * 3);
    if (x === 0) {
-    console.log('Rock');
+    computerChoice = 'Rock';
    } else if (x === 1) {
-    console.log('Paper');
+    computerChoice = 'Paper';
    } else {
-    console.log('Scissors');
+    computerChoice = 'Scissors';
    }
+   return computerChoice;
 }
-console.log(getComputerChoice())
+console.log(getComputerChoice());
 
 let toss = prompt ('Choose Rock Paper or Scissors', 'Rock');
 
 function getHumanChoice(toss) {
     if (toss === 'Rock') {
-        console.log('Rock')
+        humanChoice = 'Rock'
     } else if (toss === 'Paper') {
-        console.log('Paper')
+        humanChoice = 'Paper'
     } else {
-        console.log ('Scissors')
+        humanChoice = 'Scissor'
     }
+    return humanChoice;
 }
 console.log(getHumanChoice(toss))
 
 let humanScore = Number(0)
 let computerScore = Number(0)
-
